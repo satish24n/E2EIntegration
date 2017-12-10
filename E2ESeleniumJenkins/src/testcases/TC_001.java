23da9a14d36369e7168d7968b95cccf88d9c44d0
satish24n <android24n@gmail.com>
2017-12-11 00:02:43 +0530

    Creating Automation push.

---
+++ E2ESeleniumJenkins/src/testcases/TC_001.java
@@ -0,0 +1,26 @@
+package testcases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_001 {
+	
+	@Test
+	public void test1() {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.get("https://www.facebook.com/");
+		driver.findElement(By.id("email")).sendKeys("satishkumar24@gmail.com");
+		driver.findElement(By.id("pass")).sendKeys("techlogi");
+		driver.findElement(By.id("loginbutton")).click();
+		
+		driver.quit();
+		
+	}
+	
+}
